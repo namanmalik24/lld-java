8d5e5f3acd53d9a0ed6fd9aebf1b19ebc273c28d
skills-introduction-to-github <naman.m888@gmail.com>
2026-03-15 04:59:41 +0530

    improved code

--- src/com/lld/designpatterns/factory/client/Main.java
+++ src/com/lld/designpatterns/factory/client/Main.java
@@ -1,17 +1,20 @@
 package com.lld.designpatterns.factory.client;
 
 import com.lld.designpatterns.factory.factory.VehicleFactory;
-import com.lld.designpatterns.factory.product.Vehicle;
+import com.lld.designpatterns.factory.product.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Vehicle v1 = VehicleFactory.createVehicle("CAR");
-        v1.drive();
+        VehicleFactory.registerVehicle("CAR", Car::new);
+        VehicleFactory.registerVehicle("BIKE", Bike::new);
 
-        Vehicle v2 = VehicleFactory.createVehicle("BIKE");
-        v2.drive();
+        Vehicle car = VehicleFactory.createVehicle("CAR");
+        car.drive();
+
+        Vehicle bike = VehicleFactory.createVehicle("BIKE");
+        bike.drive();
 
     }
 
--- src/com/lld/designpatterns/factory/factory/VehicleFactory.java
+++ src/com/lld/designpatterns/factory/factory/VehicleFactory.java
@@ -1,20 +1,28 @@
 package com.lld.designpatterns.factory.factory;
 
-import com.lld.designpatterns.factory.product.*;
+import com.lld.designpatterns.factory.product.Vehicle;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class VehicleFactory {
 
+    private static final Map<String, Supplier<Vehicle>> registry = new HashMap<>();
+
+    public static void registerVehicle(String type, Supplier<Vehicle> creator) {
+        registry.put(type.toUpperCase(), creator);
+    }
+
     public static Vehicle createVehicle(String type) {
 
-        if (type.equalsIgnoreCase("CAR")) {
-            return new Car();
+        Supplier<Vehicle> supplier = registry.get(type.toUpperCase());
+
+        if (supplier == null) {
+            throw new IllegalArgumentException("Vehicle type not supported");
         }
 
-        if (type.equalsIgnoreCase("BIKE")) {
-            return new Bike();
-        }
-
-        throw new IllegalArgumentException("Unknown vehicle type");
+        return supplier.get();
     }
 
 }
